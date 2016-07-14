88061c18cf14cc0e4a82900af7a7ba9efe51695c
Christian Fobel <christian@fobel.net>
2016-07-14 16:03:52 -0400

    Refactor canvas and control handle references

--- planeTransform.js
+++ planeTransform.js
@@ -1,15 +1,16 @@
 class planeTransform{
-
-    constructor(){
+    constructor(canvas_element, control_handles_element){
         //datgui is messing up the camera to world space action
         //if two points are the same, the texture disappears
         //plane jumps to other side
         this.frame = 0;
         this.displayHandles = true;
         this.updatePos = true;
+        this.canvas_element = canvas_element;
+        this.control_handles_element = control_handles_element;
         document.addEventListener("keydown", (event)=>this.onDocumentKeyDown(event), false);
         document.addEventListener("keyup", (event)=>this.onDocumentKeyUp(event), false);
-        
+
         this.init();
     }
 
@@ -39,18 +40,19 @@ class planeTransform{
             y: .1 * height
         }];
 
-        var canvas = document.getElementById("canvasID");
-        $(canvas).attr("width", width);
-        $(canvas).attr("height", height);
-        $("#controlHandles").width(width);
-        $("#controlHandles").height(height);
-        
-        this.pointsUI = d3.controlPointsUI()(d3.select('#controlHandles')
-                                            .selectAll('circle')
-                                            .data(this.controlPoints)).radius(10);
-        
+        $(this.canvas_element).attr("width", width);
+        $(this.canvas_element).attr("height", height);
+        $(this.control_handles_element).width(width);
+        $(this.control_handles_element).height(height);
+
+        this.pointsUI =
+            d3.controlPointsUI()(d3.select(this.control_handles_element)
+                                 .selectAll('circle')
+                                 .data(this.controlPoints)).radius(10);
+
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(50, width/height, 1, 10000);
+        this.camera = new THREE.PerspectiveCamera(50, width / height, 1,
+                                                  10000);
         this.camera.position.z = this.sceneWidth;
         this.scene.add(this.camera);
 
@@ -63,7 +65,7 @@ class planeTransform{
             }, function(error){
             console.log("Failed to get a stream due to", error);
         });
-          
+
         if(!this.video.src){
             console.log("no data from webcam");
         }
@@ -80,11 +82,28 @@ class planeTransform{
             },
         };
 
+        var vertexShader = `
+varying vec4 textureCoord;
+attribute float diagonalRatio;
+void main() {
+    textureCoord = vec4(uv.xy, 0.0, 1.0);
+    textureCoord.w = diagonalRatio;
+    textureCoord.xy *= textureCoord.w;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}`;
+
+        var fragmentShader = `
+uniform sampler2D uSampler;
+varying vec4 textureCoord;
+void main() {
+    gl_FragColor = texture2D(uSampler, textureCoord.xy/textureCoord.w);
+}`;
+
         customMaterial = new THREE.ShaderMaterial({
             uniforms: customUniforms,
-            vertexShader: document.getElementById('vertex_shader').textContent,
-            fragmentShader: document.getElementById('fragment_shader').textContent,
-            side: THREE.DoubleSide
+            side: THREE.DoubleSide,
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader
         });
 
         //draw the plane and add diagonal ratios
@@ -93,85 +112,40 @@ class planeTransform{
         for (var i = 0; i < 4; i++) {
             diagonalRatios[0] = 1.0;
         }
-        this.geometry.addAttribute('diagonalRatio', new THREE.BufferAttribute(diagonalRatios, 1));
+        this.geometry.addAttribute('diagonalRatio', new
+                                   THREE.BufferAttribute(diagonalRatios, 1));
 
         this.mesh = new THREE.Mesh(this.geometry, customMaterial);
         this.scene.add(this.mesh);
-        this.renderer = new THREE.WebGLRenderer({
-            canvas: canvas
-        });
+        this.renderer = new THREE.WebGLRenderer({canvas: this.canvas_element});
         document.body.appendChild(this.renderer.domElement);
         this.renderer.render(this.scene, this.camera);
-        
-        
-        var diagonalRatios = this.calculateDiagonalRatios();
-        if(diagonalRatios){
-            for (var i = 0; i < 4; i++) {
-                this.geometry.attributes.diagonalRatio.array[i] = diagonalRatios[i];
-            }
-        }
-        this.pointsUI.on("changed", (d)=>this.onControlPointChange(d));
-    }
-    
-    onControlPointChange(d){
-        //d consists of points x, y and index
-        var pos = [];
-        var vectorSet = [];
-        
-        
-        //canvas space to camera space
-        for (var i = 0; i < this.controlPoints.length; i++) {
-            vectorSet[i] = new THREE.Vector3((this.controlPoints[i].x / this.sceneWidth) * 2 - 1,
-                                             -(this.controlPoints[i].y / this.sceneHeight) * 2 + 1, 0.5);
-            vectorSet[i].unproject(this.camera);
-            var dir = vectorSet[i].sub(this.camera.position).normalize();
-            var distance = -this.camera.position.z / dir.z;
-            pos[i] = this.camera.position.clone().add(dir.multiplyScalar(distance));
-        }
-        
-        var posArray = [];
-        
-        //turn pos (THREE.vector array) into a array
-        for(var i = 0; i < pos.length; i++){
-            posArray.push(pos[i].x);
-            posArray.push(pos[i].y);
-        }
-        
-        if(!this.prevPos)
-            this.prevPos = $.extend(true, [], posArray);
-        var transform = PerspT(posArray, this.prevPos).coeffsInv;
-        this.prevPos = $.extend(true, [], posArray);
-        
-        var geoPos = this.geometry.attributes.position.array;
-        if(this.updatePos){
-            [geoPos[3], geoPos[4]]  = this.getUpdatedPos(geoPos[3], geoPos[4], transform);
-            [geoPos[9], geoPos[10]] = this.getUpdatedPos(geoPos[9], geoPos[10], transform);
-            [geoPos[6], geoPos[7]] = this.getUpdatedPos(geoPos[6], geoPos[7], transform);
-            [geoPos[0], geoPos[1]] = this.getUpdatedPos(geoPos[0], geoPos[1], transform);
-        }
-        
-        //new diagonal ratios
-        var diagonalRatios = this.calculateDiagonalRatios();
-        if(diagonalRatios){
-            for (var i = 0; i < 4; i++) {
-                this.geometry.attributes.diagonalRatio.array[i] = diagonalRatios[i];
-            }
-        }
 
+
+        var diagonalRatios = this.calculateDiagonalRatios();
+        if(diagonalRatios){
+            for (var i = 0; i < 4; i++) {
+                this.geometry.attributes.diagonalRatio.array[i] = diagonalRatios[i];
+            }
+        }
+        this.pointsUI.on("changed", (d) => this.onControlPointChange(d));
+    }
+
+    onControlPointChange(d){
     }
 
     listen() {
-        requestAnimationFrame(()=>this.listen());
-        
+        requestAnimationFrame(() => this.listen());
+
         //datgui handling
         if(this.displayHandles)
-            $("#controlHandles").show();
+            $(this.control_handles_element).show();
         else
-            $("#controlHandles").hide();
-            
+            $(this.control_handles_element).hide();
+
         this.frame += 1;
         this.render();
-        
+
     }
 
     render() {
@@ -182,14 +156,14 @@ class planeTransform{
         this.geometry.attributes.diagonalRatio.needsUpdate = true;
         this.renderer.render(this.scene, this.camera);
     }
-                    
+
     onDocumentKeyDown(event){
         var keyCode = event.which;
         if(keyCode == 16){
             this.updatePos = false;
         }
     }
-    
+
     onDocumentKeyUp(event){
         var keyCode = event.which;
         if(keyCode == 16){
@@ -200,7 +174,7 @@ class planeTransform{
     calculateDiagonalRatios(){
         if(this.geometry === undefined)
             return false;
-          
+
         var tr, br, tl, bl;
         tr = [this.geometry.attributes.position.array[3], this.geometry.attributes.position.array[4]];
         br = [this.geometry.attributes.position.array[9], this.geometry.attributes.position.array[10]]
@@ -214,39 +188,39 @@ class planeTransform{
 
         var intx = (tr[1] - tl[1] - (slope1*tr[0] - slope2*tl[0]))/(slope2 - slope1);
         var inty = slope1*(intx - tr[0]) + tr[1];
-        
+
         var dis1 = this.calculateDistance(intx, inty, tr[0], tr[1]);
         var dis2 = this.calculateDistance(intx, inty, br[0], br[1]);
         var dis3 = this.calculateDistance(intx, inty, tl[0], tl[1]);
         var dis4 = this.calculateDistance(intx, inty, bl[0], bl[1]);
-        
+
         if(!(dis1 && dis2 && dis3 && dis4))
             return false;
 
         return [(dis2 + dis3)/dis2, (dis1 + dis4)/dis4, (dis1 + dis4)/dis1, (dis2 + dis3)/dis3];
     }
-  
+
     calculateDistance(x1, y1, x2, y2){
         return Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
     }
-    
+
     getUpdatedPos(x, y, M){
         var W = x*M[6] + y*M[7] + M[8];
         var X = x*M[0]/W + y*M[1]/W + M[2]/W;
         var Y = x*M[3]/W + y*M[4]/W + M[5]/W;
         return [X, Y];
     }
-    
+
     rotateLeft(){
         this.controlPoints.unshift(this.controlPoints.pop());
         this.onControlPointChange(null);
     }
-    
+
     rotateRight(){
         this.controlPoints.push(this.controlPoints.shift());
         this.onControlPointChange(null);
     }
-    
+
     flipHorizontal(){
         //1234-> 4321
         this.rotateRight();
@@ -255,7 +229,7 @@ class planeTransform{
         this.controlPoints[3] = temp;
         this.onControlPointChange(null);
     }
-    
+
     flipVertical(){
         //1234-> 2143
         this.rotateLeft();
@@ -265,4 +239,4 @@ class planeTransform{
         this.onControlPointChange(null);
     }
 }
-  
+
